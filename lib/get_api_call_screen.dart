d3537ce0bc1b69026bfa63033d9d380de581d8e2
KshitijH123 <kshitijhapase@gmail.com>
2024-09-11 12:28:09 +0530

    category comntroller

--- lib/get_api_call_screen.dart
+++ lib/get_api_call_screen.dart
@@ -1,21 +1,21 @@
-import 'package:category_services/api_call_controller/get_api_call_service.dart';
+import 'package:category_services/api_call_controller/get_api_call_controller.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatelessWidget {
-  final CategoryService categoryController = Get.put(CategoryService());
+  final CategoryController categoryController = Get.put(CategoryController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Category List'),
+        title: const Text('Category List'),
       ),
       body: Obx(() {
         if (categoryController.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         } else if (categoryController.categories.isEmpty) {
-          return Center(child: Text('No categories found.'));
+          return const Center(child: Text('No categories found.'));
         } else {
           return ListView.builder(
             itemCount: categoryController.categories.length,
